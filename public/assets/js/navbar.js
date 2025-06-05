fe55428cfc3dce1c73219960245bc8d07ece0fbe
Magaliek24 <magalie.kniola@gmail.com>
2025-06-05 11:15:11 +0200

    Structure de base ok

---
+++ public/assets/js/navbar.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle_button = document.querySelector(".navbar .toggle");
+  const nav_links = document.querySelector(".navbar .nav-links");
+  console.log(nav_links);
+
+  if (toggle_button) {
+    toggle_button.addEventListener("click", () => {
+      toggle_button.classList.toggle("active");
+    });
+  }
+});
